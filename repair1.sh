b48d0b7c5a5360af328c5f8f7f69743e7e94b279
Noge <146479712+nogebeat@users.noreply.github.com>
2024-10-26 00:22:56 +0200

    Create repair1.sh

---
+++ repair1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+clear
+echo "DEMARRAGE DU SCRIPT DE REPARATION DU CODING STYLE "
+sleep 5
+clear 
+echo "Installation de l'utilitaire  !!!!! Préparez vous à ecrire votre mot de passe !!! "
+
+sudo apt purge build-essentials -y
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install build-essentials -y
+
+clear
+
+echo "REPARATION TERMINE VEILLER ATTENDRE"
+
+
+echo "5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "6"
+
+sleep 1
+echo "Orhhh je blague " 
+sleep 1
+
+exit 
